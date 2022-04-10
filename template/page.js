334f1d2b51f6185881cfbc9d34a55e235d64552a
H52e5 <2737164694@qq.com>
2022-04-10 20:41:01 +0800

    chore: updata style

--- template/page.js
+++ template/page.js
@@ -1,4 +1,4 @@
-fetch('./rss.json')
+fetch('https://h52e5.github.io/sina-news/rss.json')
 .then(async function (response) {
   const res = await response.json();
   const items = res.items;
@@ -10,7 +10,7 @@ fetch('./rss.json')
     const li = document.createElement('li');
     const p = document.createElement('p');
     const timeObj = new Date(i.date_modified);
-    p.innerHTML = `${i.title} （<a href="${i.url}" target="_blank">${timeObj.getHours()}:${timeObj.getMinutes()}</a>）`;
+    p.innerHTML = `${i.title} <i style='color: #004'>更新时间: ${timeObj.getFullYear()}-${timeObj.getMonth()}-${timeObj.getDate()} ${timeObj.getHours()}:${timeObj.getMinutes()} </i><a href="${i.url}" target="_blank">原文链接</a>`;
     li.appendChild(p);
     fragment.appendChild(li);
   });
